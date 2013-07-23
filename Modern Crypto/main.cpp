a1196e68ed6c3e51ec892319d8947f1ad99575e8
Jeff Phillips <jmanis2@yahoo.com>
2013-07-23 21:14:04 +0200

    Initial Commit

---
+++ Modern Crypto/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Modern Crypto
+//
+//  Created by Jeff Phillips on 7/23/13.
+//  Copyright (c) 2013 Jeff Phillips. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
